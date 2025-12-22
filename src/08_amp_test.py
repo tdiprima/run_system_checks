fac11c366ff36413d7d43fa5adb86d1d66a6bc5f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-12-22 15:10:51 -0500

    fix warnings

--- src/08_amp_test.py
+++ src/08_amp_test.py
@@ -1,14 +1,12 @@
 # ⚡️ Can your system handle AMP right now?
 import torch
-from torch.cuda.amp import autocast, GradScaler
-
 print("CUDA:", torch.cuda.is_available())
-scaler = GradScaler()
+scaler = torch.amp.GradScaler('cuda')
 
 x = torch.randn(32, 1024, device="cuda")
 w = torch.randn(1024, 1024, device="cuda")
 
-with autocast():
+with torch.amp.autocast('cuda'):
     y = x @ w
 
 print("AMP test passed")
